7e9af7477337b943fdd6b1cc025d516d501510ef
NandarLinn <nandar.linn.1304@gmail.com>
2022-03-11 14:06:55 +0630

    object detection on flutter

---
+++ lib/main.dart
@@ -0,0 +1,213 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Object Detection',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File _image;
+  List _recognitions;
+  bool _busy;
+  double _imageWidth, _imageHeight;
+
+  final picker = ImagePicker();
+
+  // this function loads the model
+  loadTfModel() async {
+    await Tflite.loadModel(
+      model: "assets/yolov2_tiny.tflite",
+      labels: "assets/yolov2_tiny.txt",
+    );
+  }
+
+  // this function detects the objects on the image
+  detectObject(File image) async {
+    var recognitions = await Tflite.detectObjectOnImage(
+        path: image.path, // required
+        model: "YOLO",
+        imageMean: 127.5,
+        imageStd: 127.5,
+        threshold: 0.5, // defaults to 0.1
+        numResultsPerClass: 5, // defaults to 5
+        asynch: true // defaults to true
+        );
+    FileImage(image)
+        .resolve(ImageConfiguration())
+        .addListener((ImageStreamListener((ImageInfo info, bool _) {
+          setState(() {
+            _imageWidth = info.image.width.toDouble();
+            _imageHeight = info.image.height.toDouble();
+          });
+        })));
+    setState(() {
+      _recognitions = recognitions;
+    });
+    print("Recognition $_recognitions");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _busy = true;
+    loadTfModel();
+    setState(() {
+      _busy = false;
+    });
+    // loadTfModel().then((val) {
+    //   {
+    //     setState(() {
+    //       _busy = false;
+    //     });
+    //   }
+    // });
+  }
+
+  // display the bounding boxes over the detected objects
+  List<Widget> renderBoxes(Size screen) {
+    if (_recognitions == null) return [];
+    if (_imageWidth == null || _imageHeight == null) return [];
+
+    double factorX = screen.width;
+    double factorY = _imageHeight / _imageHeight * screen.width;
+
+    Color blue = Colors.blue;
+
+    return _recognitions.map((re) {
+      return Container(
+        child: Positioned(
+            left: re["rect"]["x"] * factorX,
+            top: re["rect"]["y"] * factorY,
+            width: re["rect"]["w"] * factorX,
+            height: re["rect"]["h"] * factorY,
+            child: ((re["confidenceInClass"] > 0.50))
+                ? Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: blue,
+                      width: 3,
+                    )),
+                    child: Text(
+                      "${re["detectedClass"]} ${(re["confidenceInClass"] * 100).toStringAsFixed(0)}%",
+                      style: TextStyle(
+                        background: Paint()..color = blue,
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    ),
+                  )
+                : Container()),
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    List<Widget> stackChildren = [];
+
+    stackChildren.add(Positioned(
+      // using ternary operator
+      child: _image == null
+          ? Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Please Select an Image"),
+                ],
+              ),
+            )
+          : // if not null then
+          Container(child: Image.file(_image)),
+    ));
+
+    stackChildren.addAll(renderBoxes(size));
+
+    if (_busy) {
+      stackChildren.add(Center(
+        child: CircularProgressIndicator(),
+      ));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Object Detector"),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            heroTag: "Fltbtn2",
+            child: Icon(Icons.camera_alt),
+            onPressed: getImageFromCamera,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            heroTag: "Fltbtn1",
+            child: Icon(Icons.photo),
+            onPressed: getImageFromGallery,
+          ),
+        ],
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          children: stackChildren,
+        ),
+      ),
+    );
+  }
+
+  // gets image from camera and runs detectObject
+  Future getImageFromCamera() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print("No image Selected");
+      }
+    });
+    detectObject(_image);
+  }
+
+  // gets image from gallery and runs detectObject
+  Future getImageFromGallery() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print("No image Selected");
+      }
+    });
+    detectObject(_image);
+  }
+}
